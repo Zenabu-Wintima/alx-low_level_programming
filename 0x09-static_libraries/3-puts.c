79a3abe2fb021c10b8ba8829c5433fed9c2f1554
Zenabu Wintima <bukarizainab21@gmail.com>
2023-11-05 12:03:39 -0500

    static

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string followed by a new line to stdout
+ * @str: The string to be printed
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
