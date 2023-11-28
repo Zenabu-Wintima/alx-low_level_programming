f2e96ed06a912015abc92f1a94102b5403d169a2
Zenabu Wintima <bukarizainab21@gmail.com>
2023-11-28 04:53:07 -0500

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 /**
 * main - prints the name of the file
 *
 * Return: Always 0 (Success)
 */
-int ain(void)
+int main(void)
 {
-printf("%s\n", __FILE__);
+printf("%s\n", __FILE__);  /* Prints the name of the current source file */
 return (0);
 }
-
